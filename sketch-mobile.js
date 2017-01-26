40eed36caaeaf645398efb57fcd0715d1bd698fe
Adam Miller <babaganoosh76@gmail.com>
2017-01-26 00:53:46 -0800

    Added files via upload

---
+++ sketch-mobile.js
@@ -0,0 +1,48 @@
+var r;
+
+function setup() {
+	createCanvas(window.innerWidth, window.innerHeight);
+	r = new Roller(25);
+}
+
+function draw() {
+	r.move();
+	r.draw();
+}
+
+function Roller(rad){
+	this.x = 0;
+	this.y = window.innerHeight/2;
+	this.rad = rad;
+	var ang = -Math.PI/6
+	var vel = 0;
+
+	this.move = function(){
+		if(this.x<=0||this.x>=window.innerWidth)
+			vel=-vel;
+
+		if(rotationZ<90)
+			vel+=Math.pow(1.05, 1.05);
+		else if(rotationZ>90&&rotationZ<180)
+			vel-=Math.pow(1.05, 1.05);
+		else
+			vel/=1.1;
+		this.x+=vel;
+
+	}
+
+	this.draw = function(){
+		background('black');
+		fill('white');
+		noStroke();
+		ellipse(this.x+15, this.y, 2*this.rad, 2*this.rad);
+		quad(this.x, this.y+this.rad+1, this.x, this.y-this.rad,
+			 this.x+15, this.y-this.rad, this.x+15, this.y+this.rad);
+		quad(this.x, this.y+this.rad+1, this.x, this.y+this.rad-1,
+			 0, this.y+this.rad-1, 0, this.y+this.rad+1);
+		stroke(1);
+		ellipse(this.x, this.y, 2*this.rad, 2*this.rad);
+		fill('tan');
+		ellipse(this.x, this.y, 0.7*this.rad, 0.7*this.rad);
+	}
+};
